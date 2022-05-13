e600ae2a5d7be22ac7908d602d8c1be86c53f125
unnati1028 <unnati2810@gmail.com>
2022-05-13 16:11:09 -0500

    Update BankingApplication.java
    Update made by Unnati!

--- BankingApplication/src/main/java/com/learning/BankingApplication/BankingApplication.java
+++ BankingApplication/src/main/java/com/learning/BankingApplication/BankingApplication.java
@@ -8,6 +8,8 @@ public class BankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);
+		System.out.println("i am cool");
 	}
 
+	
 }
